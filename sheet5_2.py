168df5a95f5a21a4b8d0cdaa5b620e820084d31a
Sepehr Fazeli Shahroudi <sf07627@stud.th-deg.de>
2022-04-25 11:01:31 +0200

    task 2

---
+++ sheet5_2.py
@@ -0,0 +1,17 @@
+import random
+from sheet5_1 import norm
+
+
+def gen(x):
+    return [[random.uniform(-1, 1), random.uniform(-1, 1)] for x in range(1, x)]
+
+
+def check(x):
+    return sum([1 for y in x if norm(y, 2) <= 1])
+
+
+def Pi(x):
+    return (check(gen(x)) / x) * 4
+
+
+print(Pi(10000))
