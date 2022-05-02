685e4a642a6024b402d4e368eaf2c72ac6cce340
Sepehr Fazeli Shahroudi <sf07627@stud.th-deg.de>
2022-05-02 10:07:56 +0200

    task1

---
+++ sheet6/task1.py
@@ -0,0 +1,23 @@
+class Flower:
+    def __init__(self, name: str, number: int, price: float):
+        self._name = name
+        self._number = number
+        self._price = price
+
+    def get_name(self):
+        return self._name
+
+    def get_number(self):
+        return self._number
+
+    def get_price(self):
+        return self._price
+
+    def set_name(self, name):
+        self._name = name
+
+    def set_number(self, number):
+        self._number = number
+
+    def set_price(self, price):
+        self._price = price
